8d05c831c596a7a5f3fb5845903a99dc4f8d7c90
Axel <axel.eriksson.0693@student.uu.se>
2024-09-24 11:40:45 +0200

    Idiotsäkrat

--- Biathlon.py
+++ Biathlon.py
@@ -2,15 +2,36 @@ from random import randint
 
 shots = 5
 targets = [1,2,3,4,5]
+odds = 70
+
+def kollaTraff(odds):
+    return randint(1,100) <= odds
 
 for i in range(shots):
     print("Select target:")
-    target = int(input())- 1
-    if(randint(1,2) == 1):
+    target = -1
+    selectedTarget = -1
+    while(target == -1):
+        try:
+            selectedTarget = int(input())- 1
+        except:
+            print("Invalid target")
+            continue
+        if(selectedTarget < 0 or selectedTarget > len(targets)):
+            print("Invalid target")
+            continue
+        target = selectedTarget
+    if(kollaTraff(odds)):
+        odds -= 10
+        if(targets[target] == 0):
+            print("Hit on closed target")
+        else:
+            print("Hit")
         targets[target] = 0
-        print("Hit")
     else:
         print("Miss")
 
+score = ""
 for target in targets:
-    print("O" if target == 0 else "#")
+    score +=("O" if target == 0 else "#") + " "
+print(score)
