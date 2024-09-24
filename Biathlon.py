e1e5d9078ef80591a4b1f50ca5846126a8d31133
Axel <axel.eriksson.0693@student.uu.se>
2024-09-24 11:55:36 +0200

    Massa funktioner

--- Biathlon.py
+++ Biathlon.py
@@ -1,26 +1,35 @@
 from random import randint
 
-shots = 5
+shots = 0
 targets = [1,2,3,4,5]
 odds = 70
 
 def kollaTraff(odds):
     return randint(1,100) <= odds
 
-for i in range(shots):
-    print("Select target:")
-    target = -1
+def selectInput(min,max, type):
     selectedTarget = -1
-    while(target == -1):
+    while(selectedTarget == -1):
+        print("Select " + str(type) + ":")
         try:
             selectedTarget = int(input())- 1
         except:
-            print("Invalid target")
+            print("Invalid input")
             continue
-        if(selectedTarget < 0 or selectedTarget > len(targets)):
-            print("Invalid target")
+        if(selectedTarget < min or selectedTarget > max):
+            selectedTarget = -1
+            print("Invalid input")
             continue
-        target = selectedTarget
+    return selectedTarget
+
+def selectRounds():
+    global shots
+    shots = selectInput(1, 10, "rounds")
+
+selectRounds()
+
+for i in range(shots):
+    target = selectInput(0, len(targets), "target")
     if(kollaTraff(odds)):
         odds -= 10
         if(targets[target] == 0):
