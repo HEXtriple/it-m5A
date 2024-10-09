50602bf77cb260b7b4d1b93b777a34cca90d3a8b
Axel <axel.eriksson.0693@student.uu.se>
2024-10-09 18:14:27 +0200

    Lade till actions

--- LagraTM.py
+++ LagraTM.py
@@ -1,30 +1,28 @@
-class user:
+class User:
     def __init__(self, name, psswd):
         self.name = name
         self.psswd = psswd
         self.lager = []
-
     def kolla_upgifter(self, input_name, input_psswd):
         return True if self.name == input_name and self.psswd == input_psswd else False
-        
-    
-
     def add_item(self, vara):
         self.lager.append(vara)
         def __str__(self):
             return f"Namn: {self.name}, Lager: {self.lager}"
-
+    def remove_item(self, vara):
+        self.lager.remove(vara)
+        def __str__(self):
+            return f"Namn: {self.name}, Lager: {self.lager}"
     def return_details(self):
         return f"User: ({self.name}, Psswd {self.psswd})"
-
     def return_lager(self):
         return self.lager
 
-test_user = user("NisseNisseNils", "123")
+test_user = User("NisseNisseNils", "123")
 test_user.lager.append("Gurka")
 users = []
 users.append(test_user)
-
+current_user = User(None, None)
 
 def start_page():
     print("----------------------")
@@ -33,7 +31,6 @@ def start_page():
     print("1) Login")
     print("2) Create account")
     print("q) Quit")
-
     while True:
         choice = input("> ")
         if int(choice) == 1:
@@ -58,8 +55,7 @@ def login():
     print("---------------")
     print("Please enter details")
     user_name = str(input("User: "))
-    user_passwd = str(input("Password: "))
-    
+    user_passwd = str(input("Password: "))   
     for user in users:
         if user_name == user.name:
             while True:
@@ -71,7 +67,7 @@ def login():
                     print("password incorrect")
                     user_passwd = str(input("Re-enter password: "))
                     continue
-            
+           
         else:
             continue
 
@@ -81,33 +77,47 @@ def create_account():
     print("Please enter details")
     user_name = str(input("User: "))
     user_passwd = str(input("Password: "))
-
-
-    new_user = user(user_name, user_passwd)
+    new_user = User(user_name, user_passwd)
     users.append(new_user)
-
     print(new_user.return_details(), "has been created")
 
 
 def action(user):
     print("Select an action")
+    print("1) Add item")
+    print("2) Remove item")
+    print("3) Log out")
     choice = input("")
+    match choice:
+        case "1":
+            itemToAdd = input(">")
+            user.add_item(itemToAdd)
+        case "2":
+            itemToRemove = user.lager[int(input("Index of item: ")) - 1]
+            user.remove_item(itemToRemove)
+        case "3":
+            global current_user
+            current_user = None
+    print("---------------------")
+        
+
 
 def enter_lager(user):
     item_order = 0
+    print("Items:")
     for item in user.return_lager():
         item_order += 1
         print(f"{item_order}) {item}")
+    print("-------------------")
     action(user)
 
 def system():
+    global current_user
     current_user = None
     system_is_running = True
     while system_is_running:
         if current_user != None:
             enter_lager(current_user)
-
-
         else:
             user_input = start_page()
             current_user=login() if user_input == 1 else create_account()
